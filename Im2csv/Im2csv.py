64101ebaeeeeab50b406ae23f5b31fe385fd5016
Victor Couty <victor@couty.eu>
2016-04-06 14:19:36 +0200

    Passage des noms de fichier en argument

--- Im2csv/Im2csv.py
+++ Im2csv/Im2csv.py
@@ -1,11 +1,19 @@
 #coding: utf-8
 
 import cv2
+import sys
 
-im_addr = 'img.png'
-csv_addr = 'out.csv'
+if(len(sys.argv) != 3 or '-h' in sys.argv):
+  print("Usage: python[2|3] Im2csv.py in.[jpg|bmp|png] out.csv")
+  sys.exit(-1)
+
+im_addr = sys.argv[1]
+csv_addr = sys.argv[2]
 
 img = cv2.imread(im_addr,0) #0 = cv2.IMREAD_GRAYSCALE
+
+if img == None:
+  print("Imopssible d'ouvrir l'image",im_addr)
 
 with open(csv_addr,'w') as f:
   for i in range(len(img)):
--- Im2csv/csv2Im.py
+++ Im2csv/csv2Im.py
@@ -2,10 +2,15 @@
 
 import cv2
 import numpy as np
+import sys
+
+if(len(sys.argv) != 3 or '-h' in sys.argv):
+  print("Usage: python[2|3] csv2Im.py in.csv out.[jpg|bmp|png]")
+  sys.exit(-1)
 
 
-im_addr = 'out.png'
-csv_addr = 'img.csv'
+im_addr = sys.argv[2]
+csv_addr = sys.argv[1]
 
 tab = []
 
@@ -14,4 +19,4 @@ with open(csv_addr) as f:
     tab.append(l.split(',')[:-1])
 a = np.array(tab,np.uint8)
 
-cv2.imwrite('la.png',a)
+cv2.imwrite(im_addr,a)
