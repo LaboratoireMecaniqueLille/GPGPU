55b18e55907b21573a343333f983a97666b59fba
Victor Couty <victor@couty.eu>
2016-04-27 16:00:43 +0200

    Ajout de commentaire

--- CUcorrel/CUcorrel.h
+++ CUcorrel/CUcorrel.h
@@ -5,7 +5,7 @@
 #define HEIGHT 2048
 #define IMG_SIZE (WIDTH*HEIGHT)
 #define BLOCKSIZE 1024 //La taille du bloc pour les calculs de sommes (pour les moindres carrés)
-#define LVL 6
+#define LVL 6 // Le nombre d'étage de la pyramide
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
