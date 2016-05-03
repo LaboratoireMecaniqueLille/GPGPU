8ce57bebc59ecabde07d16d1c62ee514f98680a0
Victor Couty <victor@couty.eu>
2016-05-03 15:50:06 +0200

    Ajout de constantes de préprocesseur pour le mappage

--- CUcorrel/CUcorrel.h
+++ CUcorrel/CUcorrel.h
@@ -3,9 +3,13 @@
 #define PARAMETERS 2 // Le nombre de paramètres (et donc de champs)
 #define WIDTH 2048
 #define HEIGHT 2048
+#define NTILES 16
 #define IMG_SIZE (WIDTH*HEIGHT)
+#define T_WIDTH (WIDTH/NTILES)
+#define T_HEIGHT (HEIGHT/NTILES)
+#define T_SIZE (T_WIDTH*T_HEIGHT)
 #define BLOCKSIZE 1024 //La taille du bloc pour les calculs de sommes (pour les moindres carrés)
-#define LVL 6 // Le nombre d'étage de la pyramide
+#define LVL 4 // Le nombre d'étage de la pyramide
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
