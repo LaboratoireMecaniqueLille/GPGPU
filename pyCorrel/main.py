1f82d50d703bb8058ff66e24280cc58836fb7e93
Victor Couty <victor@couty.eu>
2016-05-25 15:28:13 +0200

    Organizing and commenting main

--- pyCorrel/main.py
+++ pyCorrel/main.py
@@ -19,10 +19,22 @@ nty = 16
 stages = 7
 img = [openImage("../Images/ref2.png")]
 
+# -- Class used to resize the images using hardware linear interpolation --
 R = Resize()
+
+# -- Resampling the original image stages times --
 for i in range(stages):
   img.append(R.resize(img[i]))
 
+correl = [0]*stages
+maxRes=800
+
+# -- creating the table of stages gridCorrel instances --
+for stage in range(stages):
+  correl[stage] = gridCorrel(img[stage],ntx,nty,verbose=1,maxRes=maxRes)
+
+# -- Starting the critical part (to be run at the fastest possible refresh rate)
+# -- Resampling the second image --
 img_d = [openImage("../Images/ref2_d.png")]
 t1 = time()
 for i in range(stages):
@@ -30,11 +42,6 @@ for i in range(stages):
 t2 = time()
 print("Resizing:",1000*(t2-t1),"ms.")
 
-correl = [0]*stages
-maxRes=800
-
-for stage in range(stages):
-  correl[stage] = gridCorrel(img[stage],ntx,nty,verbose=1,maxRes=maxRes)
 
 df=np.zeros((ntx,nty,2),np.float32)
 for stage in reversed(range(stages)):
@@ -45,4 +52,4 @@ for stage in reversed(range(stages)):
   t2 = time()
   print("stage",stage,"duration:",1000*(t2-t1),"ms.")
 
-  correl[stage].showDisplacement()
+correl[0].showDisplacement()
